ed420d81097068bb5d0c076ffdefb1e0aeead7ad
syles-le <skle717@gmail.com>
2022-12-29 17:58:15 -0700

    Created random computer choice function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let answers = ["rock", "paper", "scissors"];
-function getComputerChoice(ans) {
-    answers[Math.floor(Math.random() * answers.length)];
-    return answers
-}
-console.log(getComputerChoice)
+function computerPlay() {
+    let randomNum = Math.floor(Math.random() * 3);
+        switch (randomNum) {
+            case 0: 
+                return "rock";
+                break;
+            case 1: 
+                return "scissors";
+                break;
+            case 2:
+                return "paper";
+                break;
+             }
+};
+
