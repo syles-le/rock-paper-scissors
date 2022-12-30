ce4a84c99fdaabd59f5f98975e5a49add4994939
syles-le <skle717@gmail.com>
2022-12-29 20:00:56 -0700

    Created losing scenarios for player with function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//Create array of choices to pull from randomly for computer
+//Create function to play 1 round
+//Keep track of player/computer score, increment and decrement as needed
+//Loop through 5 rounds, declare winner
+
+
+
+
+
 const choice = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
@@ -5,6 +14,22 @@ function getComputerChoice() {
     let computerChoice = play[randomIndex];
     return computerChoice
 };
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection == playerSelection) {
+        console.log("It's a tie!");
+        return 0;
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        console.log("Rock beats scissors - You Lost!");
+        return -1;
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        console.log("Paper beats rock - You Lost!");
+        return -1;
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        console.log("Scissors beats paper - You Lost!");
+        return - 1;
+    } 
+}
 
 
 
